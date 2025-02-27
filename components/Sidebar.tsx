6276a1ef9b40f8c441a811b6d16ba1c05adaa4b8
kasoma <kasomaibrahim@gmail.com>
2025-02-27 22:21:01 +0000

    fixing style issues on nav and headerbox

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,41 +1,45 @@
 "use client";
+
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import { usePathname } from "next/navigation";
 
-function Sidebar({ user }: SiderbarProps) {
+const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
+
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
-        <Link href="/" className="mb-2 cursor-pointer items-center gap-2 flex">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
           <Image
             src="/icons/logo.svg"
-            alt="pesa logo"
-            height={34}
             width={34}
+            height={34}
+            alt="Pesa logo"
             className="size-[24px] max-xl:size-14"
           />
-          <h1 className="sidebar-logo">Pasa</h1>
+          <h1 className="sidebar-logo">Pesa</h1>
         </Link>
         {sidebarLinks.map((item) => {
           const isActive =
             pathname === item.route || pathname.startsWith(`${item.route}/`);
+
           return (
             <Link
-              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
               href={item.route}
               key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
             >
               <div className="relative size-6">
                 <Image
                   src={item.imgURL}
                   alt={item.label}
                   fill
-                  className={cn({ "brightness-[3] invert-0": isActive })}
+                  className={cn({
+                    "brightness-[3] invert-0": isActive,
+                  })}
                 />
               </div>
               <p className={cn("sidebar-label", { "!text-white": isActive })}>
@@ -49,6 +53,6 @@ function Sidebar({ user }: SiderbarProps) {
       FOOTER
     </section>
   );
-}
+};
 
 export default Sidebar;
